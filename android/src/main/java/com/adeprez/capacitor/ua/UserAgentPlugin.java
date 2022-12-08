6b346401bfe0d00f494034a9f2f7e96a73479231
Alexis Deprez <deprez.alexis@laposte.net>
2022-12-08 13:12:33 +0100

    Android implementation

--- android/src/main/java/com/adeprez/capacitor/ua/UserAgentPlugin.java
+++ android/src/main/java/com/adeprez/capacitor/ua/UserAgentPlugin.java
@@ -1,29 +1,34 @@
 package com.adeprez.capacitor.ua;
 
+import android.webkit.WebSettings;
+
+import com.getcapacitor.Bridge;
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.adeprez.capacitor.ua.UserAgent;
 
 @CapacitorPlugin(name = "UserAgent")
 public class UserAgentPlugin extends Plugin {
 
     @PluginMethod
     public void get(PluginCall call) {
-
-        call.resolve("test");
+        call.resolve(bridge.getWebView().getSettings().getUserAgentString());
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void set(PluginCall call) {
-        String userAgent = call.getString("userAgent", "");
-
+        String ua = call.getString("userAgent", null);
+        bridge.getWebView().getSettings().setUserAgentString(ua);
         call.resolve();
     }
 
     @PluginMethod(returnType = PluginMethod.RETURN_NONE)
     public void reset(PluginCall call) {
-
+        bridge.getWebView().getSettings().setUserAgentString(null);
         call.resolve();
     }
 
